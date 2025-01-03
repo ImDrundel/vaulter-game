00f1c821e2f7649fb38d0426fa4a8efc81be3171
ImDrundel <you@example.comandcher53@gmail.com>
2025-01-03 12:31:48 +0200

    feat: add multi-platforms and interaction with them

--- app/modules/GameWindow.tsx
+++ app/modules/GameWindow.tsx
@@ -1,9 +1,15 @@
 "use client"
 import { useEffect } from "react"
 import styles from "./GameWindow.module.scss"
-// interface Keys{
-
-// }
+interface Platform {
+  x: number
+  y: number
+  width: number
+  height: number
+  leftBorder: number
+  rightBorder: number
+  topBorder: number
+}
 export default function GameWindow() {
   useEffect(() => {
     const canvas = document.getElementById(
@@ -21,8 +27,8 @@ export default function GameWindow() {
         width: 30,
         height: 100,
         speed: 3,
-        gravity: 2,
-        jumpHeight: 400,
+        gravity: 4,
+        jumpHeight: 250,
         jumpSpeed: 4,
       }
       const playerCoords = {
@@ -31,35 +37,94 @@ export default function GameWindow() {
         top: player.y,
         bottom: player.y + player.height,
       }
-      const platformSize = {
-        x: 450,
-        y: 450,
-        width: 150,
-        height: 10,
-      }
-      const platformCoords = {
-        leftBorder: platformSize.x,
-        rightBorder: platformSize.x + platformSize.width,
-        topBorder: platformSize.y,
-        // bottom: ,
-      }
+
+      // const platformSize = {
+      //   x: 450,
+      //   y: 450,
+      //   width: 150,
+      //   height: 10,
+      // }
+      // const platformCoords = {
+      //   leftBorder: platformSize.x,
+      //   rightBorder: platformSize.x + platformSize.width,
+      //   topBorder: platformSize.y,
+      //   // bottom: ,
+      // }
+
+      const arrStaticPlatforms: Array<Platform> = [
+        {
+          x: 750,
+          y: 400,
+          width: 150,
+          height: 10,
+          get leftBorder() {
+            return this.x
+          },
+          get rightBorder() {
+            return this.x + this.width
+          },
+          get topBorder() {
+            return this.y
+          },
+        },
+        {
+          x: 250,
+          y: 550,
+          width: 150,
+          height: 10,
+          get leftBorder() {
+            return this.x
+          },
+          get rightBorder() {
+            return this.x + this.width
+          },
+          get topBorder() {
+            return this.y
+          },
+        },
+        {
+          x: 450,
+          y: 500,
+          width: 150,
+          height: 10,
+          get leftBorder() {
+            return this.x
+          },
+          get rightBorder() {
+            return this.x + this.width
+          },
+          get topBorder() {
+            return this.y
+          },
+        },
+      ]
+
       let onPlatform = false
-      const keysHold: { [key: string]: boolean } = {}
+      let onGround = true
 
       function drawPlayer(ctx: CanvasRenderingContext2D) {
         ctx.fillStyle = "green"
         ctx.fillRect(player.x, player.y, player.width, player.height)
       }
-      function drawPlatform() {
-        ctx!.fillStyle = "#eee"
-        ctx!.fillRect(
-          platformSize.x,
-          platformSize.y,
-          platformSize.width,
-          platformSize.height
-        )
-        requestAnimationFrame(drawPlatform)
+      // function drawPlatform() {
+      //   ctx!.fillStyle = "#eee"
+      //   ctx!.fillRect(
+      //     platformSize.x,
+      //     platformSize.y,
+      //     platformSize.width,
+      //     platformSize.height
+      //   )
+      //   requestAnimationFrame(drawPlatform)
+      // }
+      function drawAllPlatform() {
+        arrStaticPlatforms.forEach((platform) => {
+          ctx!.fillStyle = "#eee"
+          ctx!.fillRect(platform.x, platform.y, platform.width, platform.height)
+        })
+
+        requestAnimationFrame(drawAllPlatform)
       }
+
       function updatePlayerCoords() {
         playerCoords.left = player.x
         playerCoords.right = player.x + player.width
@@ -67,6 +132,9 @@ export default function GameWindow() {
         playerCoords.bottom = player.y + player.height
         requestAnimationFrame(updatePlayerCoords)
       }
+
+      //Controls
+      const keysHold: { [key: string]: boolean } = {}
 
       window.addEventListener("keydown", (e) => {
         keysHold[e.key] = true
@@ -93,45 +161,77 @@ export default function GameWindow() {
         // console.log(playerCoords)
         // console.log(platformCoords)
       }
-      function falling() {
-        if (
-          playerCoords.bottom == platformCoords.topBorder &&
-          ((playerCoords.left > platformCoords.leftBorder &&
-            playerCoords.left < platformCoords.rightBorder) ||
-            (playerCoords.left > platformCoords.leftBorder &&
-              playerCoords.right < platformCoords.rightBorder) ||
-            (playerCoords.right > platformCoords.leftBorder &&
-              playerCoords.right < platformCoords.rightBorder))
-        ) {
-          player.gravity = 0
-          onPlatform = true
+      // function falling() {
+      //   if (
+      //     playerCoords.bottom == platformCoords.topBorder &&
+      //     ((playerCoords.left > platformCoords.leftBorder &&
+      //       playerCoords.left < platformCoords.rightBorder) ||
+      //       (playerCoords.left > platformCoords.leftBorder &&
+      //         playerCoords.right < platformCoords.rightBorder) ||
+      //       (playerCoords.right > platformCoords.leftBorder &&
+      //         playerCoords.right < platformCoords.rightBorder))
+      //   ) {
+      //     player.gravity = 0
+      //     onPlatform = true
+      //   } else {
+      //     player.gravity = 2
+      //     onPlatform = false
+      //   }
+
+      //   if (player.y <= canvas!.height - 100) {
+      //     player.y = player.y + player.gravity
+      //   } else {
+      //     onPlatform = true
+      //   }
+      //   drawPlayer(ctx!)
+      //   requestAnimationFrame(falling)
+      // }
+      function fallingAll() {
+        onPlatform = false
+        arrStaticPlatforms.forEach((platform) => {
+          if (
+            playerCoords.bottom == platform.topBorder &&
+            ((playerCoords.left > platform.leftBorder &&
+              playerCoords.left < platform.rightBorder) ||
+              (playerCoords.left > platform.leftBorder &&
+                playerCoords.right < platform.rightBorder) ||
+              (playerCoords.right > platform.leftBorder &&
+                playerCoords.right < platform.rightBorder))
+          ) {
+            player.gravity = 0
+            onPlatform = true
+          }
+        })
+
+        if (player.y <= canvas!.height - player.height) {
+          player.y = player.y + player.gravity
+          onGround = false
         } else {
-          player.gravity = 2
-          onPlatform = false
+          onGround = true
         }
 
-        if (player.y <= canvas!.height - 100) {
-          player.y = player.y + player.gravity
-        } else {
-          onPlatform = true
+        if (!onPlatform || !onGround) {
+          player.gravity = 2
         }
         drawPlayer(ctx!)
-        requestAnimationFrame(falling)
+        requestAnimationFrame(fallingAll)
       }
-
-      //Controls
       window.addEventListener("keydown", (e) => {
-        if (e.key === " " && onPlatform == true) {
+        if (e.key === " " && (onPlatform == true || onGround == true)) {
           // if (player.y > canvas.height - player.jumpHeight - player.y) {
           //   player.y = player.y - player.jumpSpeed
           // }
           player.y = player.y - player.jumpHeight
         }
       })
+
+      // Context checking
       if (ctx) {
         moving()
-        falling()
-        drawPlatform()
+        // falling()
+        fallingAll()
+        // drawPlatform()
+        drawAllPlatform()
         updatePlayerCoords()
 
         //Main code end
